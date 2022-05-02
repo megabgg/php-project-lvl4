cb408a51aa67b94d2bcef6ad5924a6f485982b0f
Andrey Gorshkov <megabgg@yandex.ru>
2022-05-03 02:06:40 +0300

    Edit home page

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,15 +1,11 @@
 @extends('layouts.app')
 
 @section('content')
-    <div class="container">
-        <div class="row justify-content-center">
-            <div class="p-5 mb-4 bg-light border rounded-3">
-                <div class="container-fluid py-5">
-                    <h1 class="display-5 fw-bold">Менеджер задач</h1>
-                    <p class="col-md-8 fs-4">Четвертый проект профессии "PHP-разработчик" учебной платформы Hexlet</p>
-                    <a href="https://ru.hexlet.io/programs/php/projects/57" class="btn btn-primary btn-lg" type="button">Узнать больше</a>
-                </div>
-            </div>
+    <div class="p-5 mb-4 bg-light border rounded-3">
+        <div class="container-fluid py-5">
+            <h1 class="display-5 fw-bold">Привет от Хекслета!</h1>
+            <p class="col-md-8 fs-4">Практические курсы по программированию</p>
+            <button href="https://hexlet.io" class="btn btn-primary btn-lg" type="button">Узнать больше</button>
         </div>
     </div>
 @endsection
